4cceeac08b7bcddc442281c4f87641b2e1429cb8
jeongjoonsup <penny4860@gmail.com>
2018-12-01 13:17:39 +0900

    greeting logic comment

--- gretting.js
+++ gretting.js
@@ -5,6 +5,7 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+// web browser 에 {USER_LS: text} 를 저장
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
@@ -16,17 +17,22 @@ function handleSubmit(event) {
   saveName(currentValue);
 }
 
+// 2. USER_LS 에 내용이 없으면 input form을 display 하고 event listener를 mapping
 function askForName() {
   form.classList.add(SHOWING_CN);
+  // submit event 에 대해서 handleSubmit() 함수를 mapping
   form.addEventListener("submit", handleSubmit);
 }
 
+// 3. USER_LS 에 내용이 있으면 input form을 가리고, 
+// greeting tag 에 내용을 출력
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
   greeting.innerText = `Hello ${text}`;
 }
 
+// 1. local storage 에 저장되어있는지 여부로 분기
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
