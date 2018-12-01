95157a47f6a0fda1cdb9727a035f0ceed87f3e9e
jeongjoonsup <penny4860@gmail.com>
2018-12-01 11:25:03 +0900

    JS : 시간을 가져와서 HTML문서를 채우는 주석

--- clock.js
+++ clock.js
@@ -1,6 +1,9 @@
-const clockContainer = document.querySelector(".js-clock"),
-  clockTitle = clockContainer.querySelector("h1");
 
+// 1. js-clock class에 h1 tag document 를 가져온다.
+const clockContainer = document.querySelector(".js-clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+// 2. clockTitle 로 접근한 html 문서의 내용을 시간으로 채우는 함수.
 function getTime() {
   const date = new Date();
   const minutes = date.getMinutes();
@@ -9,8 +12,10 @@ function getTime() {
   clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+// 3. 
 function init() {
   getTime();
 }
 
+// 4. 함수를 실행
 init();
