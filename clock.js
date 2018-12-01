f8e32b83bf282d9addc704e51d2d1c73e8f1452e
jeongjoonsup <penny4860@gmail.com>
2018-12-01 11:42:44 +0900

    js : 출력 format 주석

--- clock.js
+++ clock.js
@@ -9,6 +9,8 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
+
+  // text 출력 포맷 : clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }:${seconds < 10 ? `0${seconds}` : seconds}`;
